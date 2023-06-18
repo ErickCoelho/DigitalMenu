4c1fc78a03e3c95b0c62dab088664a40ee03bf81
Erick Coelho <erickcoelho06@gmail.com>
2023-06-17 23:03:23 -0300

    Header finish

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+/*const header = document.querySelector('.header');
+const headerHeight = header.offsetHeight;
+
+const menu = document.querySelector('.menu');
+menu.style.marginTop = headerHeight + 'px';*/
